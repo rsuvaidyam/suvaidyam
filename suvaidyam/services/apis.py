bf7a72602a642aee95b9384d052c933e280dfd7c
vishsuvaidyam <vish.suvaidyam@gmail.com>
2024-07-27 18:47:17 +0530

    add dashboard card

--- suvaidyam/services/apis.py
+++ suvaidyam/services/apis.py
@@ -16,3 +16,17 @@ def get_dr():
 def get_sub_dr():
     data = frappe.db.get_list('DS', fields=['name', 'name1'])
     return data
+
+
+@frappe.whitelist()
+def get_count():
+    sql = f"""
+      SELECT COUNT(*) AS total_count FROM `tabBeneficiary`;
+    """
+    return frappe.db.sql(sql, as_dict=True)
+
+
+# @frappe.whitelist()
+# def get_beneficiary_count():
+#     total_count = frappe.db.count('Beneficiary')
+#     return total_count
